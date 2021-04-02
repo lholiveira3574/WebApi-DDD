528331da281b97a4fc62e1c4f9b1741257faa02a
Luiz Henrique <luizhenrique3574@gmail.com>
2021-04-02 20:10:56 -0300

    Instalação e configuração do Swagger

--- src/WebApi.Application/Startup.cs
+++ src/WebApi.Application/Startup.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using WebApi.CrossCutting.DependecyInjection;
 
 namespace application
@@ -22,6 +24,27 @@ namespace application
             ConfigureService.ConfigureDependenciesService(services);
             ConfigureRepository.ConfigureDependenciesRepository(services);
             services.AddControllers();
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WebApi AspNetCore",
+                    Description = "Arquitetura DDD",
+                    TermsOfService = new Uri("http://www.google.com.br"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Nome",
+                        Email = "email@mail.com",
+                        Url = new Uri("http://www.google.com.br")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Termo de Licença de Uso",
+                        Url = new Uri("http://www.google.com.br")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,6 +54,12 @@ namespace application
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApi AspNetCore");
+                c.RoutePrefix = string.Empty;
+            });
 
             //app.UseHttpsRedirection();
 
